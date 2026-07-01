7c0f4b338a4204e229e61e69505fad1d578aa229
Ashir Khan <ashir3862@gmail.com>
2026-07-02 00:51:31 +0500

    Add JS variables and data types practice

---
+++ 01-variables.js
@@ -0,0 +1,44 @@
+// 27-jun-2026
+
+// -----------------------------------------------------------------------------------------------------------------------
+
+// ************************************* 1. VARIABLES  ********************
+
+// var = function scope, allow Re-declaration
+// let & const = blocked scope, don't allow Re-declaration
+// --------------------------------------------------
+
+// let can't declared again after once but can assign value
+// -------------------------------------------------
+
+// initilize means only assign 1st time value & assign value means after initialization
+// ------------------------------------------
+
+// console.log(num1); // -------> RefrenceError (not defined)
+// -----------------------------------------
+
+// console.log(typeof num1); // -------> undefined
+// ------------------------------------------
+
+// let num2;
+// console.log(num2); // -------> undefined
+// -------------------------------------------
+
+// let num = 100
+// let num = 200
+// console.log(num); // ---> SyntaxError: Identifier 'num' has already been declared
+// ----------------------------------------------------------
+
+// let name = "ashir25" // declare and initialize value
+// name = "ashir"  // assign value
+// console.log(name); // ---------> ashir
+// ----------------------------------------------------------
+
+// const a1 = 123
+// a1 = 23 // -----> Type Error
+// console.log(a1);
+// ------------------------------------------------------------
+
+// const a;
+// console.log(a); // SyntaxError: Missing initializer in const declaration
+// -------------------------------------------------------------
---
+++ 02-datatypes.js
@@ -0,0 +1,60 @@
+// ****************************************** 2. DATA TYPES  **********************
+
+// ####     Primitive     ####
+
+// const var1 = "hello world"
+// console.log(typeof var1); // string
+
+// const var2 = 22.5
+// console.log(typeof var2); // number
+
+// const var3 = 122
+// console.log(typeof var3); // number
+
+// const login = true
+// console.log(typeof login); // boolean
+
+// let a;
+// console.log(typeof a); // undefined
+
+// let user = null
+// console.log(user); // null
+// console.log(typeof user); // object
+
+// const a1 = Symbol(123)
+// const a2 = Symbol(123)
+// console.log(a1 == a2); // -------> false
+
+// --------------------------------------------------------
+
+// ####    Non-Primitive      ####
+
+// const obj = {
+//     name: "ashir",
+//     age: 21
+// }
+// console.log(obj);
+// console.log(typeof obj); // object
+
+// const arr = [1,2,3,4,5]
+// console.log(arr);
+// console.log(typeof arr); // ---> object
+
+// function greet(){
+//     console.log("hello Ashir");
+// }
+//  console.log(greet()); // ---> undefined because value not return
+
+// ----------------------------------------------
+
+// function greet(){
+//     return("hello Ashir khan");
+// }
+// let msg = greet()
+// console.log(msg); // hello Ashir khan
+// console.log(typeof msg); // string
+// console.log(greet); // ---> function body pass in console
+// console.log(typeof greet); // ---> function
+// console.log(typeof greet()); // ---> string
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------
