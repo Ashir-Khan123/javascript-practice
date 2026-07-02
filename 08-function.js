3d6b85c134be77bcd1848b12c66fc24e9b813b50
Ashir Khan <ashir3862@gmail.com>
2026-07-02 22:23:56 +0500

    functions completed

--- 08-function.js
+++ 08-function.js
@@ -1,3 +1,7 @@
+// -------------------------------------
+// Function Declaration
+// ---------------------------------
+
 // function myName(){
 //     console.log("A")
 //     console.log("s")
@@ -7,7 +11,7 @@
 // }
 // myName()
 
-// // --------------------
+// --------------------
 
 // function sum(num1, num2){
 //     console.log(num1 + num2);
@@ -29,7 +33,7 @@
 //     // if(username === undefined){
 //     //     console.log('please enter a username');
 //     // }
-//     // OR 
+//     // OR
 //     if(!username){
 //         console.log('please enter a username');
 //         return
@@ -40,10 +44,141 @@
 // console.log(result); // undefined
 
 // --------------------------------------------
-//  2/july/2026
+//  2 / july / 2026
 // ==========================================
+// Rest operator
+// function shoppingCart(...num1){
+//     console.log(num1); // [220, 5300, 475]
+// }
+// shoppingCart(220, 5300, 475)
 
-function shoppingCart(num1){
-    console.log(num1);
-}
-shoppingCart(220, 5300, 475)
+// ----------------------------------------
+
+// const user = {
+//     name: "Ashir",
+//     price : 2000
+// }
+
+// function userData(anyObject){
+//     console.log(`username is ${anyObject.name}, price is ${anyObject.price}`);
+// }
+// userData(user)
+
+// ---------------------------------------
+
+// const arr = [200, 450, 392, 910]
+
+// function returnValue(getArr){
+//     return getArr[1]
+// }
+// // console.log(returnArr(arr));
+// console.log(returnValue([200, 100, 201]));
+
+// -----------------------------------------------------
+
+// function greeting(){
+//     console.log("AOA! Ashir");
+// }
+// greeting()
+
+// ---------------------------------
+
+// function calculator(num1 , ope , num2){
+//     if(ope == "+") return num1 + num2
+//     if(ope == "-") return num1 - num2
+//     if(ope == "*") return num1 * num2
+//     if(ope == "/") return num1 / num2
+// }
+// // let result =  calculator(25 , "+" , 4)
+// let result =  calculator(5 , "*" , 4)
+// console.log(result);
+
+// -------------------------------------
+
+// function evenOdd(num){
+//     if(!num) return "Invalid Input"
+//     if(num % 2 == 0) return 'Even Number'
+//     if(num % 2 != 0) return 'Odd Number'
+// }
+// console.log(evenOdd(52));
+
+// -----------------------------------
+
+// function factorial(num){
+//     let fact = 1
+//     for(let i = 1 ; i <= num ; i++){
+//         fact *= i
+//     }
+//     return fact
+// }
+// console.log(factorial(5))
+
+// ----------------------------------
+
+// function largestOfTwoNumbers(num1 , num2){
+//     if(num1 > num2) return `${num1} is largest`
+//     if(num1 < num2) return `${num2} is largest`
+// }
+// console.log(largestOfTwoNumbers(6, 99));
+
+// ----------------------------------
+
+// function reverseValue(str){
+//     let rev = "";
+//     for(let i = 0 ; i < str.length ; i++){
+//         rev += str[str.length - 1 - i]
+//     }
+//     return rev
+// }
+// console.log(reverseValue("2468"));
+
+// ---------------------------------
+
+// function palindrome(str) {
+//   let rev = "";
+//   for (let i = 0; i < str.length; i++) {
+//     rev += str[str.length - 1 - i];
+//   }
+//   if (str == rev) {
+//     console.log("Palindrome");
+//   } 
+//   else {
+//     console.log("Not Palindrome");
+//   }
+// }
+// palindrome("level")
+
+// -------------------------------------
+// Function Expression
+// ---------------------------------
+
+// const calculateSum = function (){
+//     return 'Hello world'
+// }
+// console.log(calculateSum());
+
+// -------------------------------------
+// IIFE (Immediately Invoked Function Expression)
+// ---------------------------------
+
+// (function(){
+//     console.log("Hi");
+// })()
+
+// -------------------------------------
+// Arrow Function
+// ---------------------------------
+
+// const reminder = (num1 , ope , num2) => {
+//     if(ope == "%") return num1 % num2
+// }
+// console.log(reminder(5 , "%" , 3));
+
+// ---------------------------------------
+
+// const multiple = (num1, num2) => num1 * num2
+// const res = multiple(3,7)
+// console.log(res);
+
+// -------------------------------------
+
