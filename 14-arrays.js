e969603668cbb0fb587c363daa66dcbb9b431c15
Ashir Khan <ashir3862@gmail.com>
2026-07-04 23:51:39 +0500

    array-methods

--- 14-arrays.js
+++ 14-arrays.js
@@ -1,4 +1,11 @@
 // Array 
 
-const myArr = [1,2,3,4,5]
-console.log(myArr[0]);
+// const myArr = [1,2,3,4,5]
+// console.log(myArr[0]); // 1
+
+const strArr = ["Ashir", "shakir" ,"suhail", "hamza", "Ahmed"]
+// console.log(strArr);
+let push = strArr.push("zain", "abbas")
+console.log(push); // return length of array   
+console.log(strArr.pop()); // return remove element 
+
