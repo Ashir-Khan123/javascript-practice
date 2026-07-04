3c7da05d94593c179c9f34a852d2a724e3c759f9
Ashir Khan <ashir3862@gmail.com>
2026-07-04 13:45:07 +0500

    add Numbers-&-Maths

---
+++ 12-nums-&-maths.js
@@ -0,0 +1,43 @@
+// --------------------------------------
+// Numbers
+// --------------------------------------
+
+// const num = 400
+// console.log(num);
+
+// const num2 = new Number(420.9261) 
+// console.log(num2.toString().length) // 3
+// console.log(num2.toFixed(2)); // 420.93 -----> digits after decimal 
+// console.log(num2.toPrecision(3)); 
+// let amount = 500000
+// console.log(amount.toLocaleString()); // 500,000 (US standards) 
+
+// --------------------------------------
+// Maths
+// --------------------------------------
+
+// console.log(Math.abs(-22)); // 22 (convert only neg to pos)
+// console.log(Math.round(89.6)); // 90
+// console.log(Math.ceil(4.1)); // 5
+// console.log(Math.floor(4.8)); //4
+// console.log(Math.min(3,4,5,6)); // 3
+// console.log(Math.max(3,4,5,6)); // 6
+
+// console.log(Math.random()); // value btwwen 0 and 1
+// console.log(Math.floor(Math.random() * 10 + 1));
+
+// let min = 30
+// let max = 50
+
+// console.log(Math.floor(Math.random() * ( max - min + 1 )) + min); // 30 - 49
+
+// ------------------------------------------------
+// Tasks
+// ------------------------------------------------
+
+// let min = 50
+// let max = 100
+// console.log(Math.floor((Math.random() * (max - min + 1) + min))); // 50 - 100
+
+// ------------------------------------------------
+
