7219ebe204e4de12712f177db26b4298897591a9
Ashir Khan <ashir3862@gmail.com>
2026-07-05 01:13:49 +0500

    array-methods completed

--- 14-arrays.js
+++ 14-arrays.js
@@ -3,9 +3,57 @@
 // const myArr = [1,2,3,4,5]
 // console.log(myArr[0]); // 1
 
-const strArr = ["Ashir", "shakir" ,"suhail", "hamza", "Ahmed"]
+// const strArr = ["Ashir", "shakir" ,"suhail", "hamza", "Ahmed"]
+// // console.log(strArr);
+// let pushArr = strArr.push("zain", "abbas")
+// console.log(pushArr); // return length of array   
+// console.log(strArr.pop()); // return remove element 
+// console.log(strArr.unshift("ghufran")); // 7
+// console.log(strArr); // (7) ['ghufran', 'Ashir', 'shakir', 'suhail', 'hamza', 'Ahmed', 'zain']
+// console.log(strArr.shift()); // ghufran
+// console.log(strArr); // (6) ['Ashir', 'shakir', 'suhail', 'hamza', 'Ahmed', 'zain'
+
+// console.log(strArr.includes("shakir")); // true
+// console.log(strArr.indexOf("suhail")); // 2
+// console.log(strArr[5]); // undefined
+
+// const newArr = strArr.join()
+// console.log(newArr);
+
+// const strArr = ["Ashir", "shakir" ,"suhail", "hamza", "Ahmed"]
+
 // console.log(strArr);
-let push = strArr.push("zain", "abbas")
-console.log(push); // return length of array   
-console.log(strArr.pop()); // return remove element 
+// console.log(strArr.slice(1, 4)); // slice till -1 and copy of an original array 
+// console.log(strArr.splice(1,3)); // refrence of original array return remove elements ['shakir', 'suhail', 'hamza']
+// console.log(strArr); // ['Ashir', 'Ahmed'] 
+// ----------------------------------------------------------------
+// const strArr1 = ["Ashir", "shakir" ,"suhail", "hamza", "Ahmed"]
+// const strArr2 = ["Ghufran" , "hashir" , "haseeb" , "taha"]
+
+// strArr1.push(strArr2)
+// console.log(strArr1);
+
+// let combineArr = strArr1.concat(strArr2) // return new array
+// console.log(combineArr);
+
+// ==============================================
+
+// Spread operator
+// let newArr = [...strArr1 , ...strArr2]
+// console.log(newArr);
+
+// ==========================================
+
+// let arr = [1,2,3,[2,5], 6,5, [3,[4,5,2],6],9,2,1]
+// let oneArr = arr.flat(Infinity) // nested arr in one line 
+// console.log(oneArr);
+
+// console.log(Array.isArray("Ashir")); // false
+// console.log(Array.isArray(["Ashir"])); // true
+// console.log(Array.from("Ashir")); // (5) ['A', 's', 'h', 'i', 'r']
+// console.log(Array.of("Ashir")); // ['Ashir']
+
+// console.log(Array.from({name : "Ashir"})); // []
+// console.log(Array.of({name : "Ashir"})); // [{name : "Ashir"}]
+
 
