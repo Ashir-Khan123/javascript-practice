4a9bc808c266beb50b3439b5d59e1bd9f0cb365d
Ashir Khan <ashir3862@gmail.com>
2026-07-14 14:57:04 +0500

    Destructuring of object

--- 16-JSON.js
+++ 16-JSON.js
@@ -43,11 +43,64 @@
 // navbar(company = "Amazon")      
 
 // --------------------------------------------
+// Template literal interpolation object ko string mein implicitly convert karti hai, aur plain object's default string representation [object Object] hoti hai.
 
-const student = {
-    name: "Ashir",
-    age: 21,
-    skills: ["HTML", "CSS", "JavaScript"]
-};
+// const student = {
+//     name: "Ashir",
+//     age: 21,
+//     skills: ["HTML", "CSS", "JavaScript"]
+// };
 
-console.log(`object : ${student}`);
+// // console.log(`object data : ${student}`); // [object , object] ---> type , object kind
+
+// const JsonStudent = JSON.stringify(student)
+// console.log(`JSON data : ${JsonStudent}`);
+// console.log(typeof JsonStudent);
+
+// ---------------------------------------
+
+// const jsonData = `{
+//     "id": 101,
+//     "product": "Laptop",
+//     "price": 80000
+// }`;
+
+// const objData = JSON.parse(jsonData)
+// console.log(jsonData);
+// console.log(objData);
+// console.log(`Product : ${objData.product}`);
+// console.log(`Price : ${objData.price}`);
+
+// -----------------------------------------
+
+// Steps:
+// Array ko JSON string banao.
+// typeof check karo.
+// Wapas parse karo.
+// Second user's name print karo.
+
+// const users = [
+//     {
+//         id: 1,
+//         name: "Ali"
+//     },
+//     {
+//         id: 2,
+//         name: "Ahmed"
+//     },
+//     {
+//         id: 3,
+//         name: "Hamza"
+//     }
+// ];
+
+// const jsonUser = JSON.stringify(users)
+// console.log(jsonUser);
+// console.log(typeof jsonUser);
+// const objUser = JSON.parse(jsonUser)
+// console.log(objUser);
+// console.log(typeof objUser);
+// console.log(objUser[1].name);
+
+// -----------------------------------
+
