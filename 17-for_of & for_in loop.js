3c1e03b8b22d71ebd7324c2053ea913960b48465
Ashir Khan <ashir3862@gmail.com>
2026-07-15 15:33:02 +0500

    for-of and for-in completed

--- 17-for_of & for_in loop.js
+++ 17-for_of & for_in loop.js
@@ -42,3 +42,69 @@
 
 // ----------------------------------
 
+// For-of task
+
+// let departments = [
+//     {
+//         department: "Frontend",
+//         employees: [
+//             {
+//                 name: "Ali",
+//                 salary: 50000
+//             },
+//             {
+//                 name: "Ahmed",
+//                 salary: 70000
+//             }
+//         ]
+//     },
+//     {
+//         department: "Backend",
+//         employees: [
+//             {
+//                 name: "Hamza",
+//                 salary: 80000
+//             },
+//             {
+//                 name: "Usman",
+//                 salary: 60000
+//             }
+//         ]
+//     }
+// ];
+
+// for(let val of departments){
+// let dept
+// let name
+// let salary  
+//     dept = val.department
+//     console.log(`Department : ${dept}`);
+//     for(let value of val.employees){
+//         name = value.name
+//         salary = value.salary
+//         console.log(`${name} - ${salary}`);
+//     }
+//     console.log(`-----------------------------`);
+// }
+
+// --------------------------------------------
+
+// For-in taks
+
+// let product = {
+//     id : 101,
+//     name: "Laptop",
+//     price: 80000,
+//     quantity: 2,
+//     rating: 4.5,
+//     brand: "Dell"
+// };
+
+// let count = 0
+// for(let key in product){
+//     if(typeof product[key] == 'number'){
+//         count += 1
+//     }
+// }
+// console.log(`${count} numeric values`);
+
