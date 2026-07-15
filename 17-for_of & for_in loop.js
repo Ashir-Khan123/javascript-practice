1ffceefab7686116f58bb3fbb02e808289ef9483
Ashir Khan <ashir3862@gmail.com>
2026-07-15 12:06:03 +0500

    for-of & for-in task 1

---
+++ 17-for_of & for_in loop.js
@@ -0,0 +1,44 @@
+// for-of -------------> Give values
+
+// let users = [
+//     {
+//         name: "Ali",
+//         city: "Karachi"
+//     },
+//     {
+//         name: "Ahmed",
+//         city: "Lahore"
+//     },
+//     {
+//         name: "Hamza",
+//         city: "Islamabad"
+//     }
+// ];
+
+// for(let value of users){
+//     console.log(value);
+// }
+
+// for(let key in users){
+//     console.log(key);
+// }
+
+// ---------------------------------
+// For-in loop -> give keys/properties
+
+// let user = {
+//     name: "Ashir",
+//     age: 21
+// };
+
+// for(let value of user){
+//     console.log(value); // type error
+// }
+
+// for(let key in user){
+//     // console.log(user.key); // undefined
+//     console.log(user[key]);
+// }
+
+// ----------------------------------
+
