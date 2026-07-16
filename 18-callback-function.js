65c2aa21638122b492bae273b8871129ed63f795
Ashir Khan <ashir3862@gmail.com>
2026-07-16 23:12:52 +0500

    add filter() method

--- 18-callback-function.js
+++ 18-callback-function.js
@@ -92,8 +92,22 @@
 
 // -----------------------------------------
 
+let languages = [10,20,30]
 
+function customMap(arr, callback){
+    let newArr = []
+    for(let i = 0 ; i < arr.length ; i++){
+        let result = callback(arr[i])
+        newArr.push(result)
+    }
+    return newArr
+}
 
+let result = customMap(languages , (v) => {
+    return v * 2
+})
+
+console.log(result);
 
 
 
