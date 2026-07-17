8788dcd7aa3d5a32f0df54055311426d9a5fc72d
Ashir Khan <ashir3862@gmail.com>
2026-07-17 23:46:18 +0500

    reduce completed

---
+++ 22-reduce.js
@@ -0,0 +1,79 @@
+// Reduce 
+
+// const nums = [1 , 2 , 3]
+// let res = nums.reduce( function(acc , currVal){
+    //     console.log(`acc : ${acc}, curr : ${currVal}`);
+    //     return acc + currVal
+    // } , 0 )
+    
+    // console.log(res);
+    
+// let result = nums.reduce((acc , currVal) => acc + currVal , 0) // accumulator stores answer of each expression
+// console.log(result);
+
+// const shoppingCart = [
+//     {
+//         courseName : "JS Course",
+//         Price : 2999
+//     },
+//     {
+//         courseName : "pS Course",
+//         Price : 999
+//     },
+//     {
+//         courseName : "Mobile dev Course",
+//         Price : 5999
+//     },
+//     {
+//         courseName : "Data Science Course",
+//         Price : 12999
+//     },
+// ]
+
+// let totalAmount = shoppingCart.reduce((acc , currVal) => acc + currVal.Price , 0)
+// console.log(`Total Amount : ${totalAmount}`);
+
+// const nums = [5, 20, 10, 15];
+
+// let res = nums.reduce((acc , val) => {
+//     if(val > acc){
+//         acc = val
+//     }
+//     return acc
+// } , 0) // if 0 not give so acc suppose 1st value of array 
+
+// console.log(`Largest number : ${res}`);
+
+// ------------------------------------
+
+// const nums = [-5, -20, -10];
+
+// let res = nums.reduce((acc, val) => {
+//     console.log(acc , val);
+//     return val > acc ? val : acc;
+// });
+
+// console.log(res);
+
+// ------------------------------
+
+// const users = [
+//     { city: "Karachi" },
+//     { city: "Lahore" },
+//     { city: "Karachi" },
+//     { city: "Islamabad" },
+//     { city: "Karachi" }
+// ];
+
+// let result = users.reduce((acc , val) => {
+//     val = val.city
+//     if(acc[val] == undefined){
+//         acc[val] = 1
+//     }
+//     else{
+//         acc[val] += 1
+//     }
+//     return acc
+// } , {})
+
+// console.log(result);
