ee46c9b9834ae055d0f9b21a71c2780c288910db
Ashir Khan <ashir3862@gmail.com>
2026-07-20 15:48:14 +0500

    add Map DS

---
+++ 23-Set&Map DS.js
@@ -0,0 +1,24 @@
+let arr = [2 , 2 , 3 , 4 , 5 , 5 , 7]
+
+// let set = new Set(arr)
+// console.log(set)
+
+// --------------------------------
+
+// let setArr = [...new Set(arr)]
+
+// console.log(setArr);
+
+// let s = new Set()
+
+// s.add(10)
+// s.add(20)
+
+// s.delete(10)
+
+// console.log(s.has(20));
+
+// console.log(s.size);
+
+// ----------------------------
+
