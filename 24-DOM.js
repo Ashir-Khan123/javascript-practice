48034d9332893524341218d31debac8416c8d306
Ashir Khan <ashir3862@gmail.com>
2026-07-20 23:16:27 +0500

    DOM practice

---
+++ 24-DOM.js
@@ -0,0 +1,26 @@
+// document.getElementById("title")
+// <h1 id=​"title" class=​"heading">​DOM Manipulation on chai or code​</h1>​
+
+// document.getElementById("title").id
+// 'title'
+
+// document.getElementById("title").class
+// undefined
+
+// document.getElementById("title").className
+// 'heading'
+
+// document.getElementById("title").getAttribute('class')
+// 'heading'
+
+// document.getElementById("title").setAttribute('class' , "para")
+// undefined
+
+// document.getElementById("title").getAttribute('class' , 'heading test')
+// 'para'
+
+// document.getElementById("title").setAttribute('class' , 'heading para')
+// undefined
+
+// document.getElementById("title").getAttribute('class')
+// 'heading para'
